b9238ff778fc925198a06b5d13e0c80c24ba3c1e
emivega16 <emivega316@gmail.com>
2025-11-12 14:47:35 -0600

    Update  1 — Comparison Operators in Python.py

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -22,14 +22,31 @@ print(a <= b)   # True
 
 
 #predict the output of the following comparisons:
-10 > 5
-7 == 2 * 3 + 1
-8 != 8
-4 <= 2 + 2
+10 > 5  # True
+7 == 2 * 3 + 1 # True
+8 != 8 # False
+4 <= 2 + 2 # True
 
 # Write 3 examples that result in True and 3 that result in False.
-
+7 > 6
+23 > 15
+27 < 37
+79 < 35
+89 < 65
+98 < 86
 # Create a simple grade-checking condition:
 
 # practice problem :
-# where a student must check if their score is greater than or equal to 60 to pass a test.# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+# where a student must check if their score is greater than or equal to 60 to pass a test.
+score = int(input("Enter your score: "))
+if score >= 60:
+    print(You passed the test)
+else:
+    print("You did not pass the test.")
+
+# The password must be at least 8 characters long and contain at least one digit.password = "mypassword1"
+password = inpud("Enter your password:")
+if len(password) >= 8 and any(char.isdigit() for char in passward):
+    print("Password is valid.")
+else:
+    ("Password is not valid")
